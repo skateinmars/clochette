f201714fe6dda119eefb4d4c09eccf2148afad65
Jean-Philippe Moal <skateinmars@skateinmars.net>
2013-11-15 13:42:03 +0100

    Add a blank homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Clochette::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   post 'github' => 'github#hook'
 end
