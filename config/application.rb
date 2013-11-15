8d258b512745b04b1b31769db264ba363178a55f
Jean-Philippe Moal <skateinmars@skateinmars.net>
2013-11-15 13:28:43 +0100

    Do not require actionmailer

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require "active_record/railtie"
 require "action_controller/railtie"
-require "action_mailer/railtie"
+# require "action_mailer/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
